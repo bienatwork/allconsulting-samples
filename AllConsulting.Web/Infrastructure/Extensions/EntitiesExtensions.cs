31dff3ee39afbc2ccd5c134775fa14df1634b0b3
bienatwork <bienatwork@gmail.com>
2016-01-07 10:38:43 +0700

    Initial created

---
+++ AllConsulting.Web/Infrastructure/Extensions/EntitiesExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AllConsulting.Entities;
+using AllConsulting.Web.Models;
+
+namespace AllConsulting.Web.Infrastructure.Extensions
+{
+    public static class EntitiesExtensions
+    {
+        public static void UpdateOrder(this Order entity, OrderViewModel viewModel)
+        {
+            entity.CustomerNumber = viewModel.CustomerNumber;
+            entity.OrderDate = viewModel.OrderDate;
+            entity.DeliveryDate = viewModel.DeliveryDate;
+            entity.TotalPrice = viewModel.TotalPrice;
+        }
+    }
+}
