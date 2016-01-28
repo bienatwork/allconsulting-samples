5b9d181eaff19e637a9899e45dbc09fd9f1a9b6a
phanhuy91 <phan.van.huy@kloon.vn>
2016-01-28 19:34:20 +0700

    Comment Code 28/01/2016

--- AllConsulting.Web/Properties/AssemblyInfo.cs
+++ AllConsulting.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AllConsulting.Web")]
+[assembly: AssemblyTitle("ACAG.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AllConsulting.Web")]
+[assembly: AssemblyProduct("ACAG.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
